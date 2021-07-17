4cfce2152784c62bcd1a4df8abe95f186d8609aa
zaldy <zaldy@vm-debian-dev01>
2021-07-17 15:03:25 +0800

    database user validation

--- src/main/java/com/pluralsight/conference/ConferenceSecurity.java
+++ src/main/java/com/pluralsight/conference/ConferenceSecurity.java
@@ -1,5 +1,8 @@
 package com.pluralsight.conference;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,6 +15,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class ConferenceSecurity extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource dataSource;
+	
 	
 	@Override
 	protected void configure(final HttpSecurity http) throws Exception {
@@ -31,8 +38,8 @@ public class ConferenceSecurity extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("bryan").password(passwordEncoder().encode("pass")).roles("USER");
+		auth.jdbcAuthentication()
+			.dataSource(dataSource);
 	}
 	
 	@Bean
